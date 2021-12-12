e59bf39477fc63dcd355c78a368c8f5b1d88ceea
jarrospide1 <juanarrospidepons@gmail.com>
2021-12-12 11:49:50 -0800

    Upload de ejercicio Mercado Liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const { send } = require('process');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'views/home.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'views/register.html'))
+});
